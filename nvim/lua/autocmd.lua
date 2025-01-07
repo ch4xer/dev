db60384d5dc7d9d60129faade48b0ad27a893579
ch3n9w <ch4xer@gmail.com>
2025-01-07 09:08:43 +0800

    set clipboard based on SSH_TTY

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -21,13 +21,7 @@ vim.api.nvim_create_autocmd({ "InsertLeave" }, {
 })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function()
-        vim.highlight.on_yank()
-        local copy_to_unnamedplus = require("vim.ui.clipboard.osc52").copy("+")
-        copy_to_unnamedplus(vim.v.event.regcontents)
-        local copy_to_unnamed = require("vim.ui.clipboard.osc52").copy("*")
-        copy_to_unnamed(vim.v.event.regcontents)
-    end,
+    callback = vim.highlight.on_yank
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
