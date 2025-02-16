ab14dcefa8afd8c23a1f27b8c85dda97bdb0911b
ch3n9w <ch4xer@gmail.com>
2025-02-16 11:40:40 +0800

    update blink.cmp

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -31,27 +31,14 @@ return {
           border = "single",
           draw = {
             padding = 0,
-            gap = 1,
+            gap = 0,
             columns = { { "kind_icon" }, { "label", "kind", gap = 1 } },
-            components = {
-              kind_icon = {
-                ellipsis = false,
-                text = function(ctx)
-                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
-                  return kind_icon
-                end,
-                -- highlight will be provided by catppuccin
-              },
-            },
           },
         },
         documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 500,
           window = { border = "rounded" },
         },
       },
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
       keymap = {
         preset = "super-tab",
         ["<CR>"] = { "accept", "fallback" },
@@ -72,19 +59,6 @@ return {
           "snippet_backward",
           "fallback",
         },
-      },
-      sources = {
-        default = function(ctx)
-          local success, node = pcall(vim.treesitter.get_node_at_cursor)
-          if vim.bo.filetype == "markdown" then
-            return { "buffer", "path" }
-          elseif success and node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type()) then
-            return { "buffer", "path" }
-          else
-            return { "lsp", "path", "snippets", "buffer" }
-          end
-        end,
-        cmdline = {},
       },
     },
   },
