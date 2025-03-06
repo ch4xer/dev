ba775faa67636a78747e6d8fb1e7d50c5351fbf5
ch3n9w <ch4xer@gmail.com>
2025-03-06 17:57:42 +0800

    disable neovide floating shadow, to prevent laggy

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -63,4 +63,5 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animate_command_line = false
   vim.g.neovide_cursor_animate_in_insert_mode = false
   vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_floating_shadow = false
 end
