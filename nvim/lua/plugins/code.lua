c929513a1e3ab421636a5f7e6902ebb0114dad0c
ch3n9w <ch4xer@gmail.com>
2025-04-25 14:18:32 +0800

    remove avante.nvim

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -65,43 +65,6 @@ return {
     },
   },
   {
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    version = false,
-    opts = {
-      provider = "copilot",
-      behaviour = {
-        auto_apply_diff_after_generation = true,
-      },
-      hints = { enabled = true },
-      -- editor: ours = "co", theirs = "ct",
-      -- sidebar: apply_all = "A", apply = "a"
-    },
-    build = "make",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "ibhagwan/fzf-lua",
-      "zbirenbaum/copilot.lua",
-      {
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-          heading = {
-            width = "block",
-          },
-          code = {
-            style = 'normal',
-            width = "block",
-          },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
-  },
-  {
     "echasnovski/mini.ai",
     enabled = false,
   },
