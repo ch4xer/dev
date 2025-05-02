4d031d99ae672d67c9eaf58da5ee8606b7dc77b7
ch3n9w <ch4xer@gmail.com>
2025-05-03 06:26:40 +0800

    custom colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -13,4 +13,32 @@ return {
       },
     },
   },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+        term_colors = false,
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" },
+          functions = { "italic" },
+          properties = { "italic" },
+        },
+        custom_highlights = function(colors)
+          return {
+            Folded = { bg = colors.none },
+          }
+        end,
+        integrations = {
+          blink_cmp = true,
+        },
+      })
+
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
