3d32a57cba2abaab143410c7a84b291bc0edc339
ch3n9w <ch4xer@gmail.com>
2025-07-02 18:30:33 +0800

    remove neo-tree

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -48,62 +48,62 @@ return {
     },
     keys = false,
   },
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    lazy = false, -- neo-tree will lazily load itself
-    -- stylua: ignore
-    keys = {
-      { "t", function() vim.cmd("Neotree reveal toggle") end, desc = "toggle neotree" },
-    },
-    config = function()
-      require("neo-tree").setup({
-        popup_border_style = "",
-        sources = {
-          "filesystem",
-        },
-        hide_root_node = true,
-        auto_clean_after_session_restore = true,
-        close_if_last_window = true,
-        -- stylua: ignore
-        default_component_configs = {
-          indent = { padding = 0, },
-          type = { enabled = false, },
-          last_modified = { enabled = false, },
-        },
-        window = {
-          position = "float",
-          auto_expand_width = true,
-          mappings = {
-            ["t"] = "toggle_node",
-            ["<esc>"] = "cancel",
-            ["r"] = "rename",
-          },
-        },
-        filesystem = {
-          follow_current_file = {
-            enabled = true,
-            leave_dirs_open = true,
-          },
-          hijack_netrw_behavior = "open_default",
-          window = {
-            mappings = {
-              ["<2-LeftMouse>"] = "open",
-              ["<cr>"] = "open",
-              ["a"] = "add",
-              ["."] = "toggle_hidden",
-              ["y"] = "copy_to_clipboard",
-              ["s"] = "open_split",
-              ["v"] = "open_vsplit",
-              ["x"] = "cut_to_clipboard",
-              ["p"] = "paste_from_clipboard",
-              ["d"] = "delete",
-            },
-          },
-        },
-      })
-    end,
-  },
+  -- {
+  --   "nvim-neo-tree/neo-tree.nvim",
+  --   branch = "v3.x",
+  --   lazy = false, -- neo-tree will lazily load itself
+  --   -- stylua: ignore
+  --   keys = {
+  --     { "t", function() vim.cmd("Neotree reveal toggle") end, desc = "toggle neotree" },
+  --   },
+  --   config = function()
+  --     require("neo-tree").setup({
+  --       popup_border_style = "",
+  --       sources = {
+  --         "filesystem",
+  --       },
+  --       hide_root_node = true,
+  --       auto_clean_after_session_restore = true,
+  --       close_if_last_window = true,
+  --       -- stylua: ignore
+  --       default_component_configs = {
+  --         indent = { padding = 0, },
+  --         type = { enabled = false, },
+  --         last_modified = { enabled = false, },
+  --       },
+  --       window = {
+  --         position = "float",
+  --         auto_expand_width = true,
+  --         mappings = {
+  --           ["t"] = "toggle_node",
+  --           ["<esc>"] = "cancel",
+  --           ["r"] = "rename",
+  --         },
+  --       },
+  --       filesystem = {
+  --         follow_current_file = {
+  --           enabled = true,
+  --           leave_dirs_open = true,
+  --         },
+  --         hijack_netrw_behavior = "open_default",
+  --         window = {
+  --           mappings = {
+  --             ["<2-LeftMouse>"] = "open",
+  --             ["<cr>"] = "open",
+  --             ["a"] = "add",
+  --             ["."] = "toggle_hidden",
+  --             ["y"] = "copy_to_clipboard",
+  --             ["s"] = "open_split",
+  --             ["v"] = "open_vsplit",
+  --             ["x"] = "cut_to_clipboard",
+  --             ["p"] = "paste_from_clipboard",
+  --             ["d"] = "delete",
+  --           },
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
   {
     "ibhagwan/fzf-lua",
     -- stylua: ignore
@@ -176,6 +176,25 @@ return {
           end
         end, desc = "Toggle Terminal" },
     },
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts_extend = { "spec" },
+    opts = function(_, opts)
+      return {
+        preset = "helix",
+        defaults = {},
+        spec = {
+          {
+            mode = { "n", "v" },
+            { "<leader>d", group = "debug" },
+            { "<leader>f", group = "file/find" },
+            { "<leader>g", group = "git" },
+          },
+        },
+      }
+    end,
   },
   {
     "folke/todo-comments.nvim",
