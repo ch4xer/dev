17cf9c745553df629f8a23f3d7aa7d3854549106
ch3n9w <ch4xer@gmail.com>
2025-07-03 18:32:37 +0800

    disable animation for neovide

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -47,6 +47,7 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animate_command_line = false
   vim.g.neovide_cursor_animate_in_insert_mode = false
   vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_short_animation_length = 0
   vim.g.neovide_position_animation_length = 0
   vim.g.neovide_scroll_animation_far_lines = 0
   vim.g.neovide_scroll_animation_length = 0
