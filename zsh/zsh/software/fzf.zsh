331aa271bb1d32ac62ae33877ad2d7a8444aa588
ch3n9w <ch4xer@gmail.com>
2025-12-19 11:42:08 +0800

    update

--- zsh/zsh/software/fzf.zsh
+++ zsh/zsh/software/fzf.zsh
@@ -1,2 +1,2 @@
-export FZF_DEFAULT_COMMAND='fd --follow --exclude={node_modules,build,__pycache__}'
+export FZF_DEFAULT_COMMAND='fd --follow --exclude={node_modules,build,__pycache__,go.mod,go.sum}'
 export FZF_DEFAULT_OPTS="--height=100% --layout=reverse --preview='~/.config/yazi/fzf-scope.sh {}'"
