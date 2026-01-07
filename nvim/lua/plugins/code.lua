9eae5a2eb292a3475ea9a7b6ae599645000152c0
ch3n9w <ch4xer@gmail.com>
2026-01-07 17:03:54 +0800

    disable copilot limit reached message

--- nvim/lua/plugins/code.lua
+++ nvim/lua/plugins/code.lua
@@ -76,6 +76,7 @@ return {
         "zbirenbaum/copilot.lua",
         opts = {
           copilot_model = "gpt-4o-copilot",
+          disable_limit_reached_message = true
         },
       },
       {
