22d962309dd2def4e884c80f7b4eac220cc223e6
ch3n9w <ch4xer@gmail.com>
2026-04-16 22:59:07 +0800

    Improve cursor behavior across multi-line commands in zsh

--- zsh/zsh/keybinds.zsh
+++ zsh/zsh/keybinds.zsh
@@ -10,3 +10,13 @@ bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
 bindkey '^[[Z' reverse-menu-complete
 bindkey "^H" backward-kill-word
+
+# Fix cursor not being able to move across lines in multi-line commands
+bindkey '^[[D' backward-char
+bindkey '^[[C' forward-char
+bindkey '^[[A' up-line-or-history
+bindkey '^[[B' down-line-or-history
+
+# Improve Backspace and Delete behavior across lines
+bindkey '^?' backward-delete-char
+bindkey '^[[3~' delete-char
