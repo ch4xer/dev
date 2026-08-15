92d374912c7b60c3de49b4ea1a913f51f5d60a17
ch3n9w <ch4xer@gmail.com>
2026-08-15 17:50:04 +0800

    Show hovered yazi item in status bar

--- yazi/init.lua
+++ yazi/init.lua
@@ -8,5 +8,17 @@ ps.sub("ind-app-title", function(args)
 	return args
 end)
 
-require("no-status"):setup()
+for _, id in ipairs({ 1, 2, 3 }) do
+	Status:children_remove(id, Status.LEFT)
+end
+
+for _, id in ipairs({ 4, 5, 6 }) do
+	Status:children_remove(id, Status.RIGHT)
+end
+
+Status:children_add(function(self)
+	local h = self._current.hovered
+	return h and ui.Line { " ", ui.Span(ui.printable(h.name)):style(th.status.overall) } or ""
+end, 1000, Status.LEFT)
+
 require("git"):setup()
