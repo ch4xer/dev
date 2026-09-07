0a23e8f0c9ee9e100ce1ba5156d5a0beeb612d0f
ch3n9w <ch4xer@gmail.com>
2026-09-07 09:24:55 +0800

    adjust yazi statusline

--- yazi/init.lua
+++ yazi/init.lua
@@ -8,17 +8,12 @@ ps.sub("ind-app-title", function(args)
 	return args
 end)
 
-for _, id in ipairs({ 1, 2, 3 }) do
+for _, id in ipairs({ 1, 2 }) do
 	Status:children_remove(id, Status.LEFT)
 end
 
-for _, id in ipairs({ 4, 5, 6 }) do
+for _, id in ipairs({ 5, 6 }) do
 	Status:children_remove(id, Status.RIGHT)
 end
-
-Status:children_add(function(self)
-	local h = self._current.hovered
-	return h and ui.Line { " ", ui.Span(ui.printable(h.name)):style(th.status.overall) } or ""
-end, 1000, Status.LEFT)
 
 require("git"):setup()
